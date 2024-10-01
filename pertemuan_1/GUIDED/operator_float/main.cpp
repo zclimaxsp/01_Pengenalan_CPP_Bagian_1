887a6dedbe7d654d9bf0be4bfba003bc339a9d47
zclimaxsp <151652931+zclimaxsp@users.noreply.github.com>
2024-10-01 19:45:45 +0700

    Add files via upload

--- pertemuan_1/GUIDED/operator_float/main.cpp
+++ pertemuan_1/GUIDED/operator_float/main.cpp
@@ -7,13 +7,12 @@ int main()
 {
     int angka = 10;
     float desimal = 10.5;
-    string kalimat ="Naura";
+    string kalimat ="aldi";
     double tinggi = 10.4;
     char jenis_kelamin = 'P';
     bool isSunny = true;
 
     cout << "Angka: " << angka << endl;
-    cout << fixed << setprecison(1);
     cout << "Tinggi: " << tinggi << endl;
 
     return 0;
